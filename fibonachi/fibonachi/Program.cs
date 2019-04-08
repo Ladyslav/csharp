39d0c3ebada2e327a39cf7d72e9d4e42e7b5cef9
Vladyslav <vladistchenko@gmail.com>
2019-04-09 00:19:57 +0300

    Update Program.cs

--- fibonachi/fibonachi/Program.cs
+++ fibonachi/fibonachi/Program.cs
@@ -10,7 +10,7 @@ namespace fibonachi
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите конец диапазона от 1 до : ");
+            Console.Write("Введите конец диапазона от 1 до : 10");
             int end = int.Parse(Console.ReadLine());
             int j = 1;
             for (int i = 1; i <= end; i += j)
