4b8b79b66489019caee4374f79ace544dec2b4be
Vladyslav <vladistchenko@gmail.com>
2019-04-09 00:41:30 +0300

    Create Program.cs

---
+++ Lesson02/Lesson02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var human = new Human();
+            var age = human.Age;
+            human.GetAge;
+        }
+    }
+}
