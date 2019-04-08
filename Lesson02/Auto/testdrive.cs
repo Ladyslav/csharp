b1d35bf2c66fab43b22bd34d65975a85b8b55579
Vladyslav <vladistchenko@gmail.com>
2019-04-09 00:41:07 +0300

    Create testdrive.cs

---
+++ Lesson02/Auto/testdrive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class testdrive : Posluga
+    {
+        int mintime;
+        string placeForTest;
+    }
+}
