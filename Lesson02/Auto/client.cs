3e83df4cc1613e5de9665936df49838f834323b5
Vladyslav <vladistchenko@gmail.com>
2019-04-09 00:40:43 +0300

    Create client.cs

---
+++ Lesson02/Auto/client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class client : People
+    {
+        int money;
+        string license;
+    
+    }
+}
