d6c31988604df7aaf84023b316af6da20c8cf4be
Vladyslav <vladistchenko@gmail.com>
2019-04-09 00:40:51 +0300

    Create owner.cs

---
+++ Lesson02/Auto/owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class owner : People
+    {
+        
+      public  int scoreOfcar;
+
+      public  string chain;
+      public  int loot;
+    }
+}
