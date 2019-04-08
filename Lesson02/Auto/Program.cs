16ee3d286c3ecf81089cce60b47deff144fe590c
Vladyslav <vladistchenko@gmail.com>
2019-04-09 00:41:00 +0300

    Create Program.cs

---
+++ Lesson02/Auto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            owner p = new owner();
+            p.Age = 23;
+            p.Name = "Vlad";
+            p.chain = "Gold";
+            
+
+        }
+    }
+}
