f60d5104a17b3acdee7c2a105fbae5125657d6a5
Vladyslav <vladistchenko@gmail.com>
2019-04-14 16:35:57 +0300

    Create Program.cs

---
+++ Lesson02/09.04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elephant = new Slon();
+            var bird = new Bird();
+
+            var employee = new Spivrobitnik(23, "Petro");
+            var visitor = new Vidviduvach(15, "Ivan");
+
+            employee.GoToTHeZoo();
+            visitor.GoToTHeZoo();
+
+            var plant = new Roslunu();
+            var birdAviary = new Voler();
+            var elephantA= new Voler();
+            birdAviary.PlacePlant(plant);
+            elephantA.PlacePlant(plant);
+               
+
+        }
+    }
+}
