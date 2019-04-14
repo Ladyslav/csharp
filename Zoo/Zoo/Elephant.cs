c86b2b02cf5cfd5d6ff484b2a9c5835ebe9b03bb
Vladyslav <vladistchenko@gmail.com>
2019-04-14 17:18:36 +0300

    Create Elephant.cs

---
+++ Zoo/Zoo/Elephant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+   public class Elephant : Animal
+    {
+        private int _requiredCallories = 15000;
+
+        public void Eat(Food food)
+        {
+            if (food.Callories < _requiredCallories)
+                Console.WriteLine("Not enough food for elephant, require more calories.");
+            else
+                Console.WriteLine("Yea, i am not hungry anymore");
+        }
+    }
+}
