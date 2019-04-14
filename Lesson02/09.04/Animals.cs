7e3c7c29529f356fc2398c6a35a94daf25992b74
Vladyslav <vladistchenko@gmail.com>
2019-04-14 16:34:49 +0300

    Create Animals.cs

---
+++ Lesson02/09.04/Animals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._04
+{
+   public class Animals
+    {
+
+        public virtual void Eat(Korm food)
+        {
+
+        }
+    }
+}
