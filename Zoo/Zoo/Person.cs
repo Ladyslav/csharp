faf3d152a0dfc1da489b9711be49ded7a9c32e21
Vladyslav <vladistchenko@gmail.com>
2019-04-14 17:18:52 +0300

    Create Person.cs

---
+++ Zoo/Zoo/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+  public  class Person
+    {
+
+        public int Age { get; private set; }
+        public string Name { get; private set; }
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public virtual void GoToTheZoo()
+        {
+            Console.WriteLine("I am going to the zoo");
+        }
+    }
+}
