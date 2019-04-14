d896cb286aeff68058bfde8adbb7b0f561dfa495
Vladyslav <vladistchenko@gmail.com>
2019-04-14 17:18:27 +0300

    Create Visitor.cs

---
+++ Zoo/Zoo/Visitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+  public  class Visitor : Person
+    {
+        public Visitor(int age, string name) : base(age, name)
+        {
+        }
+
+        public override void GoToTheZoo()
+        {
+            base.GoToTheZoo();
+            Console.WriteLine("Horray");
+        }
+
+        public void Observe(Animal animal)
+        {
+            Console.WriteLine("Such a nice animal");
+        }
+    }
+}
