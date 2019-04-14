04f73b0480b7fa3e80e4fbc0950fe18664c61ea1
Vladyslav <vladistchenko@gmail.com>
2019-04-14 17:18:38 +0300

    Create Bird.cs

---
+++ Zoo/Zoo/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+   public class Bird : Animal
+    {
+        public override void Eat(Food food)
+        {
+            Console.WriteLine($"Yea, foor with {food.Callories} calories is good");
+        }
+    }
+}
