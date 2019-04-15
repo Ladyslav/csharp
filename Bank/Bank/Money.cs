9ce85bcd2578324059e8938702391b3efef6c007
Vladyslav <vladistchenko@gmail.com>
2019-04-15 23:40:58 +0300

    Create Money.cs

---
+++ Bank/Bank/Money.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+   public class Money
+    {
+        public Money(int scoreofmoney)
+        {
+            ScoreOfMoney = scoreofmoney;
+        }
+        public int ScoreOfMoney { get; private set; }
+
+    }
+}
