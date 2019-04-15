1f6fdd3bd6b16bf3aad23c13d421f30b09408e13
Vladyslav <vladistchenko@gmail.com>
2019-04-16 02:15:03 +0300

    Create DataBase.cs

---
+++ Game-Zombies/Game-Zombies/DataBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Zombies
+{
+  public static class DataBase
+    {
+        public static List<item> items;
+        private static List<enemy> enemies;
+
+        internal static List<enemy> Enemies { get => enemies; set => enemies = value; }
+
+        public static void Load()
+        {
+            items = new List<item>();
+            enemies = new List<enemy>();
+
+            items.Add(new item("stick", 1, true));
+            
+        }
+
+
+        public static item GetItem(int ID, int count=1)
+        {
+            item item =)tems.Find(I => I.Id == ID).Clone();
+            if (item != null)
+            {
+                if (item.isStack)
+                    item.count = count;
+                else
+                    item.count = 1;
+                return item;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static  enemy GetEnemy(int ID)
+        {
+            enemy enemy = enemies.Find(E => E.ID == ID);
+
+            if (enemy != null)
+            {
+                return enemy;
+            }
+            else
+            {
+                return null;
+            }
+        } 
+
+
+    }
+}
