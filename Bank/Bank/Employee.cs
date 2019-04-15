d0285eda6df4e2bb75e113a3a4076c58326163a5
Vladyslav <vladistchenko@gmail.com>
2019-04-15 23:40:51 +0300

    Create Employee.cs

---
+++ Bank/Bank/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Employee : People
+    {
+        public Employee(int age, string name) : base(age, name)
+        {
+        }
+    }
+}
