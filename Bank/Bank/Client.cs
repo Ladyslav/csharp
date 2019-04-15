8e0a4e0a7241ef0f282f5b75ce3ff1c77a394115
Vladyslav <vladistchenko@gmail.com>
2019-04-15 23:41:02 +0300

    Create Client.cs

---
+++ Bank/Bank/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Client : People
+    {
+        public Client(int age, string name) : base(age, name)
+        {
+        }
+        private int _scoreOfDollars = 100;
+        private int _scoreOfEuros = 150;
+
+        public override void SaySomeAboutMoney()
+        {
+            base.SaySomeAboutMoney();
+            Console.WriteLine("Got it");
+        }
+        public void Credit(Money money)
+        {
+            if (money.ScoreOfMoney > (_scoreOfDollars + _scoreOfEuros))
+            {
+                Console.WriteLine("You can get credit");
+
+            }
+            else Console.WriteLine("Sorry,you have enough of money");
+        }
+    }
+}
