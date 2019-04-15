cd290e94b657efb52d119c1515a7c8458e31578a
Vladyslav <vladistchenko@gmail.com>
2019-04-16 02:15:04 +0300

    Create Enemy.cs

---
+++ Game-Zombies/Game-Zombies/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Zombies
+{
+  public class enemy : ICloneable
+    {
+        public string Name;
+        public int ID;
+        public int MaxHealth, Health;
+        public int Damage;
+        public int Armor;
+
+
+
+        public enemy(string name, int id,int maxhealth, int damage, int armor)
+        {
+            Name = name;
+            ID = id;
+            MaxHealth = maxhealth;
+            Damage = damage;
+            Armor = armor;
+
+
+            Health = MaxHealth;
+        }
+
+        public object Clone()
+        {
+            return this; 
+        }
+    }
+}
