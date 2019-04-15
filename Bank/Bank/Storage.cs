9b36cf2d6212d4525b3d193be151001bd3e72b2f
Vladyslav <vladistchenko@gmail.com>
2019-04-15 23:40:43 +0300

    Create Storage.cs

---
+++ Bank/Bank/Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Storage
+    {
+        private Money _money;
+        
+        public bool IsClean { get; set; }
+        public void PlaceMoney(Money money)
+        {
+            _money = money;
+        }
+
+        public string GetStatus()
+        {
+            if (_money != null)
+                return "Money is inside";
+            else return "emty";
+        }
+    }
+}
