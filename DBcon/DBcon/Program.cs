d2e98c7b62fe07f72fb2db4e5ba0a5dcb4e54e01
Vladyslav <vladistchenko@gmail.com>
2019-04-22 00:10:42 +0300

    Update Program.cs

--- DBcon/DBcon/Program.cs
+++ DBcon/DBcon/Program.cs
@@ -10,6 +10,27 @@ namespace DBcon
     {
         static void Main(string[] args)
         {
+            using (GaymerContext db = new GaymerContext())
+            {
+              
+                Gaymer gaymer1 = new Gaymer { Name = "VLAD", Age = 33, Id=1, NickName="killer" };
+                Gaymer gaymer2 = new Gaymer { Name = "dIMA", Age = 26, Id=2, NickName = "Mchine" };
+
+              
+                db.Users.Add(gaymer1);
+                db.Users.Add(gaymer2);               
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+             
+                var gaymers = db.Users;
+                Console.WriteLine("Список объектов:");
+                foreach (Gaymer u in gaymers)
+                {
+                    Console.WriteLine("{0}.{1} - {3}", u.Id, u.Name, u.Age, u.NickName);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
