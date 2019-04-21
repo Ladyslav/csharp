78eab679058c080bf7bb6d601b51fb0f85092f42
Vladyslav <vladistchenko@gmail.com>
2019-04-22 00:10:50 +0300

    Update GaymerContext.cs

--- DBcon/DBcon/GaymerContext.cs
+++ DBcon/DBcon/GaymerContext.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DBcon
 {
-    class GaymerContext
+    class GaymerContext : DbContext
     {
+        public GaymerContext()
+           : base("DbConnection")
+        { }
+
+        public DbSet<Gaymer> Users { get; set; }
     }
 }
