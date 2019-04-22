6e68b3c3b81a9af89259599d2c0cf3a58349d413
Vladyslav <vladistchenko@gmail.com>
2019-04-22 23:36:40 +0300

    Create Animal.cs

---
+++ AbstrStruc/AbstrStruc/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrStruc
+{
+  abstract class Animal
+    {
+        public string Name { get; set; }
+        public string Type { get; protected set; }
+        public abstract void GetInfo();
+    }
+}
