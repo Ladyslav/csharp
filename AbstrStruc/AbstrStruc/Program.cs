e60a470d05141222b325e4b32350e85149e18635
Vladyslav <vladistchenko@gmail.com>
2019-04-22 23:36:52 +0300

    Create Program.cs

---
+++ AbstrStruc/AbstrStruc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrStruc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Parrot("Kesha"));
+            animals.Add(new Cat("Basya"));
+            foreach (Animal animal in animals)
+                animal.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
