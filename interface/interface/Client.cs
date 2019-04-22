26e1749d6063f249c003c44868f7f1904f1eb96d
Vladyslav <vladistchenko@gmail.com>
2019-04-22 23:23:55 +0300

    Update Client.cs

--- interface/interface/Client.cs
+++ interface/interface/Client.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace interface
+namespace homework
 {
-    class Client
+    class Client : Steam, User
 {
+        int _sum;
+        public string Name { get; set; }
+        public Client(string name, int sum)
+        {
+            Name = name;
+            _sum = sum;
+        }
+        public int CountOfMoney { get { return _sum; } }
+        public void Put(int sum) { _sum += sum; }
+        public void Take(int sum)
+        {
+            if (_sum >= sum)
+            {
+                _sum -= sum;
+            }
+        }
+       
 }
 }
